5587b1879a2822e6a57b9e56d59c0ce463d4b7e7
HenriqueOmena <88lmao@gmail.com>
2020-07-28 23:34:35 +0100

    finish config eslint + prettier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'working' }));
+routes.get('/', (request, response) =>
+  response.json({ message: 'working perfect' })
+);
 
 export default routes;
