6ee6760dc1627dabd4246ee7deb2a15c094b6275
HenriqueOmena <88lmao@gmail.com>
2020-10-29 00:05:06 +0000

    Add types to lib express

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
 export default function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -17,9 +22,13 @@ export default function ensureAuthenticated(
 
   try {
     const decoded = verify(token, secret);
-    console.log(decoded);
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
     return next();
   } catch {
-    throw new Error('JWT token is missing');
+    throw new Error('Invalid JWT');
   }
 }
