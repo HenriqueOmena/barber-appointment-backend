282b7cb6a3ed5e49f4457ecc4cad04d070ce0d86
HenriqueOmena <88lmao@gmail.com>
2020-11-07 22:00:51 +0000

    fix error on upload and add path to check by static route

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import './database';
 const app = express();
 
 app.use(express.json());
-app.use('files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.listen(3333, () => {
   console.log('server running');
