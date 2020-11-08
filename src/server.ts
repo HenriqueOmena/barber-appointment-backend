10a61022487e9525e26b1215562f1b9e40b5830c
HenriqueOmena <88lmao@gmail.com>
2020-11-08 21:31:56 +0000

    Global errors defined

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
 import routes from './routes';
 import uploadConfig from './config/upload';
 
@@ -11,6 +14,21 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  }
+);
 app.listen(3333, () => {
   console.log('server running');
 });
